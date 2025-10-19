65891d56a55f6072de28a611eb7c833a4db83605
Jace-Dev27 <jace.carters.email@gmail.com>
2025-10-18 21:36:29 -0700

    Made Camera Static/ Added Backdrop

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,9 +6,9 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField] private Vector3 offset = new Vector3(0f, 1f, -10f);
 
     void LateUpdate() {
-       Vector3 desiredPosition = player.position + offset;
-       Vector3 smoothed = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-       transform.position = smoothed;
+     //  Vector3 desiredPosition = player.position + offset;
+       //Vector3 smoothed = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+       //transform.position = smoothed;
 
     }
 }
